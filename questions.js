b88206f691729b9d89456d84c9cb18cc681888df
Kamila Swiader <kamila_swiader93@hotmail.com>
2023-03-28 22:26:26 +0100

    JS

---
+++ questions.js
@@ -0,0 +1,52 @@
+var questions = [
+	{
+		question: "What is the element used – and hidden – in code that explains things and makes the content more readable?",
+		answers: {
+			a: 'comparissons',
+			b: 'notes',
+			c: 'comments',
+			d: 'quotations'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "In JavaScript, what is a block of code called that is used to perform a specific task?",
+		answers: {
+			a: 'string',
+			b: 'function',
+			c: 'declaration',
+			d: 'variable'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "Where is the JavaScript placed inside an HTML document or page?",
+		answers: {
+			a: 'in the <title> section',
+			b: 'in the <meta> section',
+			c: 'in teh <body> and <head> sections',
+			d: 'in the <footer> section'
+		},
+		correctAnswer: 'c'
+	},
+	{
+		question: "What is the name of the statement that is used to exit or end a loop?",
+		answers: {
+			a: 'falter statement',
+			b: 'close statement',
+			c: 'conditional statement',
+			d: 'break statement'
+		},
+		correctAnswer: 'd'
+	},
+	{
+		question: "What is considered to be the most popular programming language in the world?",
+		answers: {
+			a: 'html',
+			b: 'swif',
+			c: 'ruby',
+			d: 'java script'
+		},
+		correctAnswer: 'd'
+	},
+];
